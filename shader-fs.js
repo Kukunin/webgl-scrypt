7d44026d55fb9f1fde7ce5ee28301ad499d19863
Sergey Kukunin <sergey.kukunin@gmail.com>
2014-07-29 23:09:34 +0300

    Move some parts of shader to functions

--- shader-fs.js
+++ shader-fs.js
@@ -3,15 +3,39 @@
 #endif
 
 vec4 toRGBA(vec2 arg) {
-      float V = float(arg.x);
-      float R = floor(V / pow(2.0, 8.0));
-      V -= R * pow(2.0, 8.0);
-      float G = V;
-      V = float(arg.y);
-      float B = floor(V / pow(2.0, 8.0));
-      V -= B * pow(2.0, 8.0);
-      float A = V;
-      return vec4(R/255., G/255., B/255., A/255.);
+    float V = float(arg.x);
+    float R = floor(V / pow(2.0, 8.0));
+    V -= R * pow(2.0, 8.0);
+    float G = V;
+    V = float(arg.y);
+    float B = floor(V / pow(2.0, 8.0));
+    V -= B * pow(2.0, 8.0);
+    float A = V;
+    return vec4(R/255., G/255., B/255., A/255.);
+}
+
+void set_nonce_to_header(out vec2 nonced_header[20], vec2 header[20], vec2 nonce[2]) {
+    /* Copy header without nonce */
+    for(int i = 0; i < 19; i++) {
+        nonced_header[i] = header[i];
+    }
+    /* Set nonce */
+    nonced_header[19] = vec2(
+        (nonce[1].y*256.) + nonce[1].x,
+        (nonce[0].y*256.) + nonce[0].x
+    );
+}
+
+void pad_the_header(out vec2 P[32], vec2 header[20]) {
+    /* Create padded message for SHA256 */
+    for(int i = 0; i < 20; i++) {
+        P[i] = header[i];
+    }
+    // Add 1 bit to the end of the message
+    P[20] = vec2(32768., 0.);
+    // Add the size of the message in bits
+    // Header is always 80 byte, so size is 640 bits
+    P[31] = vec2(0., 640.);
 }
 
 uniform vec2 base_nonce[2];
@@ -25,29 +49,13 @@ void main () {
         return;
     }
 
-    vec2 H[16]; /* Our SHA-256 hash */
+    vec2 key_hash[16]; /* Our SHA-256 hash */
+
+    vec2 nonced_header[20]; /* Header with nonce */
+    set_nonce_to_header(nonced_header, header, base_nonce);
+
     vec2 P[32]; /* Padded SHA-256 message */
-
-    vec2 B[20]; /* Header with nonce */
-    /* Copy header without nonce */
-    for(int i = 0; i < 19; i++) {
-        B[i] = header[i];
-    }
-    /* Set nonce */
-    B[19] = vec2(
-        (base_nonce[1].y*256.) + base_nonce[1].x,
-        (base_nonce[0].y*256.) + base_nonce[0].x
-    );
-
-    /* Create padded message for SHA256 */
-    for(int i = 0; i < 20; i++) {
-        P[i] = B[i];
-    }
-    // Add 1 bit to the end of the message
-    P[20] = vec2(32768., 0.);
-    // Add the size of the message in bits
-    // Header is always 80 byte, so size is 640 bits
-    P[31] = vec2(0., 640.);
+    pad_the_header(P, nonced_header);
 
     //Workaround for B[x]
     for(int i = 0; i < 32; i++) {
