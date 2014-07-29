9fadd8d536e01b1e8cbca067cddb3ab1fce94b18
Sergey Kukunin <sergey.kukunin@gmail.com>
2014-07-29 03:45:12 +0300

    Do shader only on first 40 pixels (to fit header)

--- shader-fs.js
+++ shader-fs.js
@@ -6,6 +6,12 @@ uniform vec2 base_nonce[2];
 uniform vec2 header[40];	/* Header of the block */
 
 void main () {
+    int x = int(gl_FragCoord.x);
+    if (x >= 40) {
+        gl_FragColor = vec4(0., 0., 0., 1.);
+        return;
+    }
+
     vec2 B[66];
     for(int i = 0; i < 38; i++) {
         B[i] = header[i];
@@ -13,14 +19,10 @@ void main () {
     B[38] = base_nonce[0];
     B[39] = base_nonce[1];
 
-    int x = int(gl_FragCoord.x);
-    if (x < 40) {
-        for(int i = 0; i < 40; i++) {
-            if(i == x) {
-                gl_FragColor = vec4(B[i].x/255., B[i].y/255., 0., 0.);
-            }
+    //Workaround for B[x]
+    for(int i = 0; i < 40; i++) {
+        if(i == x) {
+            gl_FragColor = vec4(B[i].x/255., B[i].y/255., 0., 0.);
         }
-    } else {
-        gl_FragColor = vec4(0., 0., 0., 1.);
     }
 }
